fc0676af1422a2aa23b261ef76d79527395ae357
adrien peligry <adrien.peligry@essec.edu>
2018-12-15 17:08:24 +0100

    andiamo

---
+++ scripts/main.js
@@ -0,0 +1,84 @@
+            
+window.onload = setTimeout(function(){
+if (confirm('Voulez-vous vous rendre sur le site xxvidsxxx ?')) {  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+
+}
+
+}, 10000);
+
+function myFunction() {
+    document.body.style.backgroundColor = "magenta";
+    document.getElementById("p1").style.color = "blue";
+        document.getElementById("p2").style.color = "blue";
+                document.getElementById("p2").style.color = "blue";
+                        document.getElementById("p3").style.color = "blue";
+        document.getElementById("p4").style.color = "blue";
+        document.getElementById("p5").style.color = "blue";
+        document.getElementById("p6").style.color = "blue";
+        document.getElementById("p7").style.color = "blue";
+        document.getElementById("p8").style.color = "blue";
+        document.getElementById("p9").style.color = "blue";
+        document.getElementById("p10").style.color = "blue";
+                document.getElementById("p11").style.color = "blue";
+
+        document.getElementById("p12").style.color = "blue";
+
+                document.getElementById("h1").style.color = "green";
+                document.getElementById("h2").style.color = "green";
+                document.getElementById("h3").style.color = "green";
+                document.getElementById("h4").style.color = "green";
+                document.getElementById("h5").style.color = "green";
+                document.getElementById("h6").style.color = "green";
+                document.getElementById("h7").style.color = "green";
+                document.getElementById("h8").style.color = "green";
+                document.getElementById("h9").style.color = "green";
+                                document.getElementById("h10").style.color = "green";
+
+
+
+    document.getElementById("p1").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p2").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p3").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p4").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p5").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p6").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p7").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p8").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p9").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p10").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p11").style.fontFamily = "papyrus,Charcoal,sans-serif";
+    document.getElementById("p12").style.fontFamily = "papyrus,Charcoal,sans-serif";
+
+
+    document.getElementById("h1").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h2").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h3").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h4").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h5").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h6").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h7").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h8").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("h9").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+    document.getElementById("10").style.fontFamily = "Comic Sans,Charcoal,sans-serif";
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
